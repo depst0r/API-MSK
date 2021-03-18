87042597b4d7bf224ac34a7b44f9e2b12bbbe547
depst0r <depstor90@gmail.com>
2021-03-18 15:57:53 +0300

    name

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,27 +1,9 @@
-import React, {useEffect} from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchPosts, fetchUsers } from '../../Redux/Action/Actions'
+import React from 'react'
 import { Cards } from '../Cards/Cards'
 
 import 'bootstrap/dist/css/bootstrap.css'
 
 export const App = () => {
-
-  const dispatch = useDispatch()
-  const selector = useSelector(state => state.rootReducer)
-
-  useEffect(() => {
-    dispatch(
-      fetchPosts(),
-      fetchUsers()
-    )
-  }, [])
-
-  useEffect(() => {
-    dispatch(
-      fetchUsers()
-    )
-  }, [])
 
   return<>
     <div className='container'>
--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { createLogger } from 'redux-logger'
+import { fetchPosts, fetchUsers } from '../../Redux/Action/Actions'
+
 
 export const Cards = () => {
-    return(
-    <div>
-    <div className="row row-cols-1 row-cols-md-2 g-4">
-  <div className="col">
-    <div className="card">
-      <img src="..." className="card-img-top" alt="..."/>
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-    </div>
-  </div>
-    </div>
-    </div>
-    )
+  
+    const dispatch = useDispatch()
+    const selector = useSelector(state => state)
+
+    useEffect(() => {
+        dispatch(
+          fetchPosts()
+        )
+      }, [])
+    
+      useEffect(() => {
+        dispatch(
+          fetchUsers()
+        )
+      }, [])
+console.log(selector?.users?.map(res => res.name))
+
+    return<>
+{ selector?.users?.map(res =>( 
+        <div className="row row-cols-1 row-cols-md-2 g-4">
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">{res.name}</h5>
+              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+        </div>
+          </div>
+))}   
+    </>
 }
