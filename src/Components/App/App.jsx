b1f4ee6ee966fa2f790976870da8e9bef42b1d59
depst0r <depstor90@gmail.com>
2021-03-18 12:14:13 +0300

    null

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchUsers, fetchPosts } from '../../Redux/Action/Actions'
 
 export const App = () => {
+
+  const dispatch = useDispatch()
+  const selector = useSelector(state => state.rootReducer)
+
+  useEffect(() => {
+    dispatch(
+      fetchUsers
+    )
+  }, [])
+
+  useEffect(() => {
+    dispatch(
+      fetchPosts
+    )
+  }, [])
+
+  console.log(fetchPosts())
+
   return<>
     
   </>
