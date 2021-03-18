442f6fd1c25b735cd14bf74f8c7418d42f2341d3
depst0r <depstor90@gmail.com>
2021-03-18 16:03:50 +0300

    btn

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -23,13 +23,16 @@ export const Cards = () => {
 console.log(selector?.users?.map(res => res.name))
 
     return<>
-{ selector?.users?.map(res =>( 
+{ selector?.users?.map(res => ( 
         <div className="row row-cols-1 row-cols-md-2 g-4">
         <div className="col">
           <div className="card">
             <div className="card-body">
+            <button className="btn btn-outline-danger">X</button>
               <h5 className="card-title">{res.name}</h5>
-              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p className="card-text">{res.email}</p>
+              <button className="btn btn-outline-info"></button>
+              <button className="btn btn-outline-dark"></button>
             </div>
           </div>
         </div>
