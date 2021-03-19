f80b8dd554c79058b553d322ca33857527735591
depst0r <depstor90@gmail.com>
2021-03-19 17:26:39 +0300

    add-material-iu

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,9 +1,30 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchPosts, fetchUsers, removeCard } from '../../Redux/Action/Actions'
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 
 export const Cards = () => {
+
+  const classes = useStyles()
   
     const dispatch = useDispatch()
     const selector = useSelector(state => state)
@@ -22,20 +43,27 @@ export const Cards = () => {
 
     return<>
 { selector?.users?.map(res => ( 
-        <div className="row row-cols-1 row-cols-md-2 g-4" key={res.id}>
-        <div className="col">
-          <div className="card">
-            <div className="card-body">
-            <button className="btn btn-outline-danger"
-              onClick={() => dispatch(removeCard(res.id))}
-            >X</button>
-              <h5 className="card-title">{res.name}</h5>
-              <p className="card-text">{res.email}</p>
-              <button className="btn btn-outline-info">Edit</button>
-            </div>
-          </div>
-        </div>
-          </div>
+         <Card className={classes.root} variant="outlined" key={res.id}>
+         <CardContent>
+           <Typography className={classes.title} color="textSecondary" gutterBottom>
+              {res.id}
+           </Typography>
+           <Typography variant="h5" component="h2">
+             {res.name}
+           </Typography>
+           <Typography className={classes.pos} color="textSecondary">
+             {res.email}
+           </Typography>
+           <Typography variant="body2" component="p">
+             well meaning and kindly.
+             <br />
+             {'"a benevolent smile"'}
+           </Typography>
+         </CardContent>
+         <CardActions>
+           <Button size="small">Delet</Button>
+         </CardActions>
+       </Card>
 ))}   
     </>
 }
