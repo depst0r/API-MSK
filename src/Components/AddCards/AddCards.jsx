3ea866c03d26c63add03ad74c90905c695eb0d1c
depst0r <depstor90@gmail.com>
2021-03-23 10:51:11 +0300

    popup

--- src/Components/AddCards/AddCards.jsx
+++ src/Components/AddCards/AddCards.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-import FiberNewIcon from '@material-ui/icons/FiberNew';
+import React, {useState} from 'react'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
+import ListIcon from '@material-ui/icons/List'
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
     button: {
       margin: theme.spacing(1),
-    },
+      borderRadius: 50,
+    }
   }));
 
 export const AddCards = () => {
-    const classes = useStyles();
+
+    const [open, setOpen] = useState(false)
+
+    const classes = useStyles()
+
+    const handleClickOpen = () => {
+        setOpen(true)
+      };
+    
+      const handleClose = () => {
+        setOpen(false)
+      }
+
 
     return (
       <div>
@@ -19,7 +38,7 @@ export const AddCards = () => {
           color="primary"
           size="small"
           className={classes.button}
-          startIcon={<FiberNewIcon />}
+          startIcon={<ListIcon />}
         >
           Add Card
         </Button>
