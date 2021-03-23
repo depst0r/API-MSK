2df17dac1f861382d1211f16bf4330ce3171155d
depst0r <depstor90@gmail.com>
2021-03-23 08:52:07 +0300

    st23

--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Pagination from '@material-ui/lab/Pagination'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        marginTop: theme.spacing(2),
+      },
+    },
+  }))
 
 export const Pagination = ({ postsPerPage, totalCards, paginate}) => {
+    
+    const classes = useStyles()
 
   const pageNumbers = []
 
@@ -11,8 +23,14 @@ export const Pagination = ({ postsPerPage, totalCards, paginate}) => {
    
 
     return<>
-        {pageNumbers?.map(res => (
-            <button onClick={() => paginate(res)}>{res}</button>
+       <div className={classes.root}>
+       {pageNumbers?.map(res => (
+           <>
+                 <Pagination count={res} shape="rounded" />
+                 <Pagination count={res} variant="outlined" shape="rounded" />
+                 </>
+            // <button onClick={() => paginate(res)}>{res}</button>
         ))}
+    </div>
     </>
 }
