65c4382a110d26082847ac8f059c3e9b6cea0402
depst0r <depstor90@gmail.com>
2021-03-24 16:28:35 +0300

    p

--- src/Components/AddCards/AddCards.jsx
+++ src/Components/AddCards/AddCards.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Button from '@material-ui/core/Button'
 import { makeStyles } from '@material-ui/core/styles'
 import ListIcon from '@material-ui/icons/List'
@@ -15,97 +15,97 @@ import Select from '@material-ui/core/Select'
 import MenuItem from '@material-ui/core/MenuItem'
 import { addCards } from '../../Redux/Action/Actions'
 
-const useStyles = makeStyles(theme => ({
-    formControl: {
-      margin: theme.spacing(1),
-      minWidth: 120,
-      width: 500
-    },
-    selectEmpty: {
-      marginTop: theme.spacing(2),
-    },
-    button: {
-        margin: theme.spacing(1),
-        borderRadius: 50,
-      }
-  }))
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    width: 500,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+  button: {
+    margin: theme.spacing(1),
+    borderRadius: 50,
+  },
+}))
 
 export const AddCards = () => {
+  const dispatch = useDispatch()
+  const selector = useSelector((state) => state)
 
-    const dispatch = useDispatch()
-    const selector = useSelector(state => state)
+  const [select, setSelect] = useState(null)
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const [open, setOpen] = useState(false)
 
-    const [select, setSelect] = useState(null)
-    const [title, setTitle] = useState('')
-    const [body, setBody] = useState('')
-    const [open, setOpen] = useState(false)
+  const classes = useStyles()
 
-    const classes = useStyles()
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
 
-    const handleClickOpen = () => {
-        setOpen(true)
-      }
-    
-      const handleClose = () => {
-        setOpen(false)
-      }
+  const handleClose = () => {
+    setOpen(false)
+  }
 
-      const handleChangeSelect = e => {
-        setSelect(e.target.value)
-      }
+  const handleChangeSelect = (e) => {
+    setSelect(e.target.value)
+  }
 
-      const handleChangeTitle = e => {
-        setTitle(e.target.value)
-      }
+  const handleChangeTitle = (e) => {
+    setTitle(e.target.value)
+  }
 
-      const handleChangeBody = e => {
-        setBody(e.target.body)
-      }
+  const handleChangeBody = (e) => {
+    setBody(e.target.body)
+  }
 
-    return (
-      <div>
-        <Button
-          variant="contained"
-          color="primary"
-          size="small"
-          className={classes.button}
-          startIcon={<ListIcon />}
-          onClick={handleClickOpen}
-        >
-          Add Card
-        </Button>
-        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+  return (
+    <div>
+      <Button
+        variant="contained"
+        color="primary"
+        size="small"
+        className={classes.button}
+        startIcon={<ListIcon />}
+        onClick={handleClickOpen}
+      >
+        Add Card
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
         <DialogTitle id="form-dialog-title">Create new card</DialogTitle>
         <DialogContent>
-        <FormControl className={classes.formControl}>
-        <InputLabel id="demo-simple-select-label">User Name</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={select}
-          onChange={handleChangeSelect}
-        >
-            {selector?.users?.map(user => (
-                <MenuItem 
-                value={user.name} 
-                key={user.id}
-                >
-                    {user?.name}
-                </MenuItem> 
-            ))}
-        </Select>
-      </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="demo-simple-select-label">User Name</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={select}
+              onChange={handleChangeSelect}
+            >
+              {selector?.users?.map((user) => (
+                <MenuItem value={user.name} key={user.id}>
+                  {user?.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
           <DialogContentText>
-          <InputLabel id="title">Title</InputLabel>
-          <Input
-            autoFocus
-            margin="dense"
-            id="title"
-            label="title"
-            fullWidth
-            value={title}
-            onChange={handleChangeTitle}
-          />
+            <InputLabel id="title">Title</InputLabel>
+            <Input
+              autoFocus
+              margin="dense"
+              id="title"
+              label="title"
+              fullWidth
+              value={title}
+              onChange={handleChangeTitle}
+            />
           </DialogContentText>
           <InputLabel id="body">Body</InputLabel>
           <Input
@@ -127,6 +127,6 @@ export const AddCards = () => {
           </Button>
         </DialogActions>
       </Dialog>
-      </div>
-    )
+    </div>
+  )
 }
--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,15 +1,12 @@
-import React, { useState } from 'react'
-import Box from '@material-ui/core/Box';
-import { Cards } from '../Cards/Cards'
-import { AddCards } from '../AddCards/AddCards'
-import { useSelector } from 'react-redux';
-import { Paginate } from '../Paginate/Paginate';
-
-
+import React, { useState } from "react"
+import Box from "@material-ui/core/Box"
+import { Cards } from "../Cards/Cards"
+import { AddCards } from "../AddCards/AddCards"
+import { useSelector } from "react-redux"
+import { Paginate } from "../Paginate/Paginate"
 
 export const App = () => {
-
-  const selector = useSelector(state => state)
+  const selector = useSelector((state) => state)
   console.log(selector)
 
   const [currentPage, setCurrentPage] = useState(1)
@@ -17,27 +14,32 @@ export const App = () => {
 
   const indexOffLastPost = currentPage * postsPerPage
   const indexOffFirstPost = indexOffLastPost - postsPerPage
-  const currentPosts = selector?.combineCards?.cards?.slice(indexOffFirstPost, indexOffLastPost)
+  const currentPosts = selector?.combineCards?.cards?.slice(
+    indexOffFirstPost,
+    indexOffLastPost
+  )
 
-  const paginate = pageNumber => setCurrentPage(pageNumber)
+  const paginate = (pageNumber) => setCurrentPage(pageNumber)
 
-
-  return<>
-  <AddCards/>
-    <Box
-    alignContent="flex-start"
-    display="flex"
-    flexWrap="wrap"
-    >
-        <Cards cards={currentPosts}/>
-    </Box>
-        <Box display="flex" justifyContent="center" m={1} p={1} bgcolor="background.paper">
-        <Paginate 
+  return (
+    <>
+      <AddCards />
+      <Box alignContent="flex-start" display="flex" flexWrap="wrap">
+        <Cards cards={currentPosts} />
+      </Box>
+      <Box
+        display="flex"
+        justifyContent="center"
+        m={1}
+        p={1}
+        bgcolor="background.paper"
+      >
+        <Paginate
           postsPerPage={postsPerPage}
           totalCards={selector?.combineCards?.cards?.length}
           paginate={paginate}
         />
       </Box>
-  </>
-
+    </>
+  )
 }
