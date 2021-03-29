22341615d046b5707b5816d41bd1b455a1f260c7
depst0r <depstor90@gmail.com>
2021-03-29 16:45:20 +0300

    arr-start

--- src/Components/UpdateCard/UpdateCard.jsx
+++ src/Components/UpdateCard/UpdateCard.jsx
@@ -1,6 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Modal from '@material-ui/core/Modal'
+import Button from '@material-ui/core/Button'
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10
+}
+
+function getModalStyle() {
+  const top = 50 + rand()
+  const left = 50 + rand()
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  }
+}
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}))
 
 export const UpdateCard = ({ arr }) => {
-  console.log('res', arr)
-  return <></>
+  const classes = useStyles()
+
+  const [modalStyle] = useState(getModalStyle)
+  const [open, setOpen] = useState(false)
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  console.log(arr)
+
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <h2 id="simple-modal-title">Text in a modal</h2>
+      <p id="simple-modal-description"></p>
+    </div>
+  )
+
+  return (
+    <div>
+      <Button size="small" color="primary" onClick={handleOpen}>
+        EDIT
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        {body}
+      </Modal>
+    </div>
+  )
 }
